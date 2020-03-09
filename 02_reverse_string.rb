375cf73fce88e919645d081f0e94dee59ab0fad2
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-03-08 22:59:00 -0400

    finished reverse string

--- 02_reverse_string.rb
+++ 02_reverse_string.rb
@@ -1,3 +1,15 @@
-def reverse_string(string)
+def reverse_string!(string)
+    left_index = 0
+    right_index = string.length - 1
 
+    while left_index < right_index
+        # placeholder = string[left_index]
+        # string[left_index] = string[right_index]
+        # string[right_index] = placeholder
+        # uses extraneous variable
+
+        string[left_index], string[right_index] = string[right_index], string[left_index]
+        left_index += 1
+        right_index -= 1
+    end
 end
