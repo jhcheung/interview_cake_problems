b07e5dddafe8329723c8c1461b833a31cebeba0d
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-10-15 23:41:13 -0400

    finished find rotation pt problem

---
+++ 13_find_rotation_point.rb
@@ -0,0 +1,68 @@
+def find_rotation_point(words)
+
+    # Find the rotation point in the array.
+    first_word = words.first
+    floor_index = 0
+    
+    ceiling_index = words.length - 1
+    
+    while floor_index < ceiling_index
+      guess_index = (floor_index + ceiling_index) / 2
+      if words[guess_index] >= first_word
+        floor_index = guess_index
+      else 
+        ceiling_index = guess_index
+      end
+      
+      return ceiling_index if floor_index + 1 == ceiling_index
+    end
+  
+    
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # Tests
+  
+  def run_tests
+    desc = 'small array'
+    actual = find_rotation_point(['cape', 'cake'])
+    expected = 1
+    assert_equal(actual, expected, desc)
+  
+    desc = 'medium array'
+    actual = find_rotation_point(['grape', 'orange', 'plum', 'radish', 'apple'])
+    expected = 4
+    assert_equal(actual, expected, desc)
+  
+    desc = 'large array'
+    actual = find_rotation_point(['ptolemaic', 'retrograde', 'supplant',
+                                  'undulate', 'xenoepist', 'asymptote',
+                                  'babka', 'banoffee', 'engender',
+                                  'karpatka', 'othellolagkage'])
+    expected = 5
+    assert_equal(actual, expected, desc)
+  
+    # Are we missing any edge cases?
+  end
+  
+  def assert_equal(a, b, desc)
+    puts "#{desc} ... #{a == b ? 'PASS' : "FAIL: #{a.inspect} != #{b.inspect}"}"
+  end
+  
+  run_tests
